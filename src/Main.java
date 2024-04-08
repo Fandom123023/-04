1be731ec3d93e61db840627f166689512608138a
Tural <musaev.tural.94@mail.ru>
2024-04-08 21:13:06 +0300

    домашка 04 доработал косметические правки внесены!!

--- src/Main.java
+++ src/Main.java
@@ -8,35 +8,32 @@ public class Main {
         int age = 18;
         if (age >= 18) {
             System.out.printf("Если возраст человека равен " + age + ", то он совершеннолетний.");
-        } if (age < 17){
-
+        }
+        if (age < 17) {
             System.out.println(" Если возраст человека равен " + age + " то он не достиг совершеннолетия , нужно не много подождать.");
         }
 
-
         System.out.println("\ntask 2");
-
         int temperature = 12;
-        if (temperature < 5)
-        {
+        if (temperature < 5) {
             System.out.println(" на улице " + temperature + " градусов, нужно надесть шапку");
-        } if (temperature > 5) {
+        }
+        if (temperature > 5) {
             System.out.println(" на улице " + temperature + " градусов, можно идти без шапки");
         }
 
-            System.out.println("\ntask3");
-            int speed = 60;
-            if (speed <= 60) {
-                System.out.printf(" если скорость " + speed + " km/h ,  то можно ездить спокойно");
-            } else {
-                System.out.printf("если скоорость " + speed + " km/h ,  то нужно заплатить штраф");
-
+        System.out.println("\ntask3");
+        int speed = 60;
+        if (speed <= 60) {
+            System.out.printf(" если скорость " + speed + " km/h ,  то можно ездить спокойно");
+        } else {
+            System.out.printf("если скоорость " + speed + " km/h ,  то нужно заплатить штраф");
         }
+
         System.out.println("\ntask4");
         int newAge = 2;
         if (newAge >= 2 && newAge <= 6) {
             System.out.printf("если возраст человека %s то он ходит в садик\n", newAge);
-
         } else if (newAge >= 7 && newAge <= 17) {
             System.out.printf("если возраст человека %s то он ходит школу\n", newAge);
         } else if (newAge >= 18 && newAge <= 24) {
@@ -49,46 +46,32 @@ public class Main {
         int newAge2 = 7;
         if (newAge2 < 5) {
             System.out.printf(" если возраст ребенка равен " + newAge2 + ", то ему нельзя кататься на аттракционе");
-        } else if (newAge2 >= 5 && newAge2 < 14)
-        {
+        } else if (newAge2 >= 5 && newAge2 < 14) {
             System.out.printf("если возраст ребенка равен " + newAge2 + ", то он может кататься со взрослыми на аттракционе");
-        } else   {
+        } else {
             System.out.printf("если возраст ребенка равен " + newAge2 + ",  то он может кататься без взрословго на аттракционе");
         }
 
         System.out.println("\ntask6");
         int passengers = 100;
-
         if (passengers <= 60) {
             System.out.println(" в вагоне есть сидячие места");
-
         } else if (passengers <= 102) {
             System.out.println("в вагоне есть место но только стоячее");
-
         } else {
             System.out.println("в вагоне уже нет места");
-
         }
 
         System.out.println("\ntask7");
-        int one =80;
+        int one = 80;
         int two = 99;
         int three = 135;
-        if (one >= two && one>=three){
+        if (one >= two && one >= three) {
             System.out.println(" наибольшее число: " + one);
         } else if (two >= one && two >= three) {
             System.out.println(" наибольшее число:" + two);
         } else {
             System.out.println(" наибольшее число:" + three);
-
-
-
-
-
-
-
         }
-
-
     }
 }
